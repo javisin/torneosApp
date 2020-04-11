a0e5a2d98b5b333ef03f3e3ca4b8a25177b6eb4a
Javier Sintes <javier@secret-source.eu>
2020-04-11 23:16:40 +0100

    Initialize notifications plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import {NavigationStart, Router, RouterEvent} from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +13,40 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  userName: string;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private notification: LocalNotifications,
+    private router: Router
   ) {
     this.initializeApp();
+    router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    ).subscribe(() => {
+      this.userName = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).nombre : null;
+      }
+    );
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.notification.requestPermission().then(() => {
+        this.notification.schedule({
+          id: 1,
+          text: 'Single Hola',
+          foreground: true
+        });
+        }
+      );
+      // @ts-ignore
+      this.notification.schedule({
+        title: 'Te amo aun mas bendacote',
+        trigger: { every: 'minute', count: 5 }
+      });
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import {ComponentsModule} from './components/components.module';
+import {ComponentsModule} from '../components/components.module';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ComponentsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ComponentsModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    LocalNotifications
   ],
   bootstrap: [AppComponent]
 })
