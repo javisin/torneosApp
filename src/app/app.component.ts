c88f9d7c4961298cb4e3556c726ec12f6c4be417
Javier Sintes <javier@secret-source.eu>
2020-04-13 23:35:26 +0100

    Request notifications permission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
-import {NavigationStart, Router, RouterEvent} from '@angular/router';
-import {filter} from 'rxjs/operators';
+import {Router} from '@angular/router';
+import {Storage} from '@ionic/storage';
+import {AppService} from '../services/app.service';
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed } from '@capacitor/core';
+const { PushNotifications } = Plugins;
+
 
 @Component({
   selector: 'app-root',
@@ -14,39 +21,66 @@ import {filter} from 'rxjs/operators';
 })
 export class AppComponent {
   userName: string;
+  token: string;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private notification: LocalNotifications,
-    private router: Router
+    private router: Router,
+    public appService: AppService,
+    private storage: Storage
   ) {
     this.initializeApp();
-    router.events.pipe(
-      filter(event => event instanceof NavigationStart)
-    ).subscribe(() => {
-      this.userName = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).nombre : null;
-      }
-    );
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-      this.notification.requestPermission().then(() => {
-        this.notification.schedule({
-          id: 1,
-          text: 'Single Hola',
-          foreground: true
-        });
-        }
-      );
-      // @ts-ignore
-      this.notification.schedule({
-        title: 'Te amo aun mas bendacote',
-        trigger: { every: 'minute', count: 5 }
-      });
+      this.storage.get('user').then(user => this.appService.updateUser(user));
+      this.appService.getUser().subscribe(user => this.userName = user ? user.nombre : null);
+      console.log('Initializing HomePage');
+
+      // Request permission to use push notifications
+      // iOS will prompt user and return if they granted permission or not
+      // Android will just grant without prompting
+      // PushNotifications.requestPermission().then( result => {
+      //   if (result.granted) {
+      //     // Register with Apple / Google to receive push via APNS/FCM
+      //     PushNotifications.register();
+      //   } else {
+      //     // Show some error
+      //   }
+      // });
+      //
+      // // On success, we should be able to receive notifications
+      // PushNotifications.addListener('registration',
+      //   (token: PushNotificationToken) => {
+      //     alert('Push registration success, token: ' + token.value);
+      //     console.log(token.value);
+      //   }
+      // );
+      //
+      // // Some issue with our setup and push will not work
+      // PushNotifications.addListener('registrationError',
+      //   (error: any) => {
+      //     alert('Error on registration: ' + JSON.stringify(error));
+      //   }
+      // );
+      //
+      // // Show us the notification payload if the app is open on our device
+      // PushNotifications.addListener('pushNotificationReceived',
+      //   (notification: PushNotification) => {
+      //     alert('Push received: ' + JSON.stringify(notification));
+      //   }
+      // );
+      //
+      // // Method called when tapping on a notification
+      // PushNotifications.addListener('pushNotificationActionPerformed',
+      //   (notification: PushNotificationActionPerformed) => {
+      //     alert('Push action performed: ' + JSON.stringify(notification));
+      //   }
+      // );
     });
   }
 }
