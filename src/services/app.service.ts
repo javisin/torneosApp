79127b30ea39423fc43aefd497b5b32ec6f0e076
Javier Sintes <javier@secret-source.eu>
2020-04-17 00:16:23 +0100

    Add user interface

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,35 +1,36 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
+import {User} from './user';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AppService {
     private readonly url: string;
-    private readonly currentUser: BehaviorSubject<any>;
+    private readonly currentUser: BehaviorSubject<User>;
 
     constructor(private http: HttpClient) {
         this.url = 'https://www.todotorneos.com/wsapp06';
-        this.currentUser = new BehaviorSubject<any>('null');
+        this.currentUser = new BehaviorSubject<User>(null);
     }
-    logIn(loginForm): Observable<any> {
+    logIn(loginForm): Observable<User> {
         const form = new FormData();
         for (const key in loginForm) {
             if (loginForm.hasOwnProperty(key)) {
                 form.append(key, loginForm[key]);
             }
         }
-        return this.http.post(`${this.url}/solologin.php`, form);
+        return this.http.post<User>(`${this.url}/solologin.php`, form);
     }
-    registerUser(registerForm): Observable<any> {
+    registerUser(registerForm): Observable<object> {
         const form = new FormData();
         for (const key in registerForm) {
             if (registerForm.hasOwnProperty(key)) {
                 form.append(key, registerForm[key]);
             }
         }
-        return this.http.post(`${this.url}/registra421.php`, form);
+        return this.http.post<object>(`${this.url}/registra421.php`, form);
     }
     getTorneos(user): Observable<any> {
         const httpOptions = {
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,2 +1,7 @@
 export interface User {
+  ape: string;
+  nick: string;
+  nombre: string;
+  token: string;
+  login?: string;
 }
