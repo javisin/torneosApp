e07a9b93da6e7615be58843a0773e153236bbf32
Javier Sintes <javier@secret-source.eu>
2020-04-21 12:10:21 +0100

    Add links for every category

--- src/pages/torneos/lista-categorias/lista-categorias.component.ts
+++ src/pages/torneos/lista-categorias/lista-categorias.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AppService} from '../../../services/app.service';
-import {NavParams} from '@ionic/angular';
+import {NavParams, PopoverController} from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-categorias',
@@ -8,11 +8,12 @@ import {NavParams} from '@ionic/angular';
   styleUrls: ['./lista-categorias.component.scss'],
 })
 export class ListaCategoriasComponent implements OnInit {
-  private idTorneo: string;
-  private categorias: any[];
+  public idTorneo: string;
+  public categorias: any[];
   constructor(
     private serv: AppService,
-    private navParams: NavParams
+    private navParams: NavParams,
+    private popoverController: PopoverController
   ) {
     this.idTorneo = this.navParams.data.torneoID;
   }
@@ -21,6 +22,9 @@ export class ListaCategoriasComponent implements OnInit {
     this.serv.getCategorias(this.idTorneo).subscribe(res => {
       this.categorias = res.categorias;
     });
+  }
+  async closePopOver() {
+    await this.popoverController.dismiss();
   }
 
 }
