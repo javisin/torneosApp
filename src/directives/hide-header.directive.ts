6ee0b82ec647b502552aac2860d25fc5bba3a4ba
Javier Sintes <javier@secret-source.eu>
2020-04-28 13:21:03 +0100

    Update hide-header directive

--- src/directives/hide-header.directive.ts
+++ src/directives/hide-header.directive.ts
@@ -13,11 +13,16 @@ import {DomController} from '@ionic/angular';
 })
 export class HideHeaderDirective implements OnInit {
   private currentScroll: number;
+  private visible: boolean;
+  private scrollDown: boolean;
+  private maxScroll: number;
   constructor(
       private el: ElementRef,
       private renderer: Renderer2,
       private dom: DomController) {
     this.currentScroll = 0;
+    this.maxScroll = 0;
+    this.visible = true;
   }
   @Input() nav: HTMLElement;
   ngOnInit() {
@@ -28,13 +33,17 @@ export class HideHeaderDirective implements OnInit {
   }
 
   @HostListener('scroll') onScroll() {
-    console.log(this.el.nativeElement.scrollTop, this.currentScroll);
-    if (this.el.nativeElement.scrollTop > this.currentScroll && this.el.nativeElement.scrollTop > 200) {
+    this.maxScroll = this.el.nativeElement.scrollHeight - this.el.nativeElement.clientHeight;
+    this.scrollDown = this.el.nativeElement.scrollTop > this.currentScroll;
+    if (this.scrollDown && this.visible && this.el.nativeElement.scrollTop > 200) {
+      this.visible = false;
       this.dom.write(() => {
         this.renderer.setStyle(this.nav, 'margin-top', `-${ this.nav.clientHeight }px`);
         this.renderer.setStyle(this.el.nativeElement, 'height', 'calc(100vh - 41px)' );
       });
-    } else {
+    } else if (!this.scrollDown && !this.visible
+        && this.el.nativeElement.scrollTop < (this.maxScroll - 200 )) {
+      this.visible = true;
       this.dom.write(() => {
         this.renderer.setStyle(this.nav, 'margin-top', '0');
         this.renderer.setStyle(this.el.nativeElement, 'height', 'calc(100vh - 141px)' );
