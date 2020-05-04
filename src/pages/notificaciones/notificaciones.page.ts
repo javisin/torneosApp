38c317d02ae8bf77680749a705d053c4f65e1030
Javier Sintes <javier@secret-source.eu>
2020-05-04 09:52:31 +0100

    Add notificaciones page

--- src/pages/notificaciones/notificaciones.page.ts
+++ src/pages/notificaciones/notificaciones.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {NotificacionService} from '../../services/notificacion/notificacion.service';
+import {UserService} from '../../services/user/user.service';
+import {User} from '../../services/user/user';
+import {AlertController} from '@ionic/angular';
+import {Storage} from '@ionic/storage';
+import {Router} from '@angular/router';
+import {createErrorAlert} from '../../helpers/createErrorAlert';
 
 @Component({
   selector: 'app-notificaciones',
@@ -6,10 +13,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notificaciones.page.scss'],
 })
 export class NotificacionesPage implements OnInit {
+  public notificaciones: any[];
+  private user: User;
 
-  constructor() { }
+  constructor(private notificacionService: NotificacionService,
+              private userService: UserService,
+              private alertController: AlertController,
+              private storage: Storage,
+              private router: Router) { }
 
   ngOnInit() {
+    this.userService.getUser().subscribe(async user => {
+      if (user !== null) {
+        this.user = user;
+        this.notificacionService.getNotificaciones(user).subscribe(res => this.checkNotificaciones(res));
+      }
+    });
+  }
+  async checkNotificaciones(res) {
+    if (res.Error) {
+      const alert = await createErrorAlert(res.Error, this.alertController, this.userService, this.storage, this.router);
+      await alert.present();
+    } else {
+      if (res.notificaciones.length > 0) {
+        this.notificaciones = res.notificaciones;
+      }
+    }
+  }
+  async doRefresh(e) {
+    this.notificacionService.getNotificaciones(this.user).subscribe(res => {
+      this.checkNotificaciones(res);
+      e.target.complete();
+    });
   }
 
 }
