5622c056ea4fdb2ba9e2dc99738078107fe2d9d8
Javier Sintes <javier@secret-source.eu>
2020-05-10 23:13:12 +0100

    Add resultado interface

---
+++ src/services/torneo/resultado.ts
@@ -0,0 +1,13 @@
+export interface Resultado {
+  Idpartido: number;
+  idequipo1: string;
+  equipo1: string;
+  rdo1: string;
+  idequipo2: string;
+  equipo2: string;
+  rdo2: string;
+  ganador: string;
+  datospartido: string;
+  fechapartido: string;
+  horapartido: string;
+}
