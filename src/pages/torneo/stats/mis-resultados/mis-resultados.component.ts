c30ac395fb0a6a141114ef6086c85910c292dafd
Javier Sintes <javier@secret-source.eu>
2020-05-14 22:43:03 +0100

    Add TorneoService on mis-resultados component

--- src/pages/torneo/stats/mis-resultados/mis-resultados.component.ts
+++ src/pages/torneo/stats/mis-resultados/mis-resultados.component.ts
@@ -1,17 +1,26 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {TorneoService} from '../../../../services/torneo/torneo.service';
+import {Resultado} from '../../../../services/torneo/resultado';
 
 @Component({
   selector: 'app-mis-resultados',
   templateUrl: './mis-resultados.component.html',
   styleUrls: ['./mis-resultados.component.scss'],
 })
-export class MisResultadosComponent implements OnInit {
-  @Input() idTorneo: string;
+export class MisResultadosComponent implements OnChanges {
+  @Input() idCategoria: string;
+  @Input() torneoDetails: any;
+  public results: Resultado[];
+  public torneoType: string;
 
   constructor(private torneoService: TorneoService) { }
 
-  ngOnInit() {
+  async ngOnChanges() {
+    if (this.torneoDetails && this.torneoDetails.idequipo !== '') {
+      const info = await this.torneoService.getMisResultados(this.idCategoria, this.torneoDetails.idequipo).pipe().toPromise();
+      this.results = info.resultados;
+      this.torneoType = info.modalidadvisual;
+    }
   }
 
 }
