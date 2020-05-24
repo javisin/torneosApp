dc0765967d80ecbee8fb534be8700348a449410e
Javier Sintes <javier@secret-source.eu>
2020-05-24 21:01:19 +0100

    Add 'sets' modality form in add-result component

--- src/pages/torneo/stats/add-result/add-result.component.ts
+++ src/pages/torneo/stats/add-result/add-result.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ModalController} from '@ionic/angular';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {AlertController, ModalController} from '@ionic/angular';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {TorneoService} from '../../../../services/torneo/torneo.service';
 
 @Component({
   selector: 'app-add-result',
@@ -8,24 +9,68 @@ import {FormBuilder, FormGroup} from '@angular/forms';
   styleUrls: ['./add-result.component.scss'],
 })
 export class AddResultComponent implements OnInit {
-  @Input() equipo1: string;
-  @Input() equipo2: string;
+  @Input() nombreEquipo1: string;
+  @Input() nombreEquipo2: string;
+  @Input() idEquipo1: string;
+  @Input() idEquipo2: string;
+  @Input() jornada: string;
+  @Input() idCategoria: string;
+  @Input() idPartido: string;
+  @Input() torneoType: string;
   public resultForm: FormGroup;
 
   constructor(private modalController: ModalController,
-              private formBuilder: FormBuilder) { }
+              private formBuilder: FormBuilder,
+              private torneoService: TorneoService,
+              private alertController: AlertController) { }
 
   ngOnInit() {
-    this.resultForm = this.formBuilder.group({
-      result1: '',
-      result2: ''
-    });
+    if (this.torneoType === 'sets') {
+      this.resultForm = this.formBuilder.group({
+        txtrdo11: ['', Validators.required],
+        txtrdo12: ['', Validators.required],
+        txtrdo13: ['', Validators.required],
+        txtrdo14: ['', Validators.required],
+        txtrdo15: ['', Validators.required],
+        txtrdo21: ['', Validators.required],
+        txtrdo22: ['', Validators.required],
+        txtrdo23: ['', Validators.required],
+        txtrdo24: ['', Validators.required],
+        txtrdo25: ['', Validators.required],
+        idequipoganador: ['', Validators.required],
+        idequipo1: this.idEquipo1,
+        idequipo2: this.idEquipo2,
+        idtorneo: this.idCategoria,
+        njornada: this.jornada,
+        idresultado: this.idPartido, // ID PARTIDO TONTO
+      });
+    } else {
+      this.resultForm = this.formBuilder.group({
+        txtrdo11: ['', Validators.required],
+        txtrdo21: ['', Validators.required],
+        idequipo1: this.idEquipo1,
+        idequipo2: this.idEquipo2,
+        idtorneo: this.idCategoria,
+        njornada: this.jornada,
+        idresultado: this.idPartido, // ID PARTIDO TONTO
+      });
+    }
   }
   async dismissModal() {
     await this.modalController.dismiss();
   }
-  onSubmit(form) {
-    console.log(form);
+  async onSubmit(form) {
+    if (this.resultForm.status === 'VALID') {
+      this.torneoService.setResult(form).subscribe();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, rellena todos los campos.',
+        buttons: ['OK'],
+        translucent: true,
+      });
+      await alert.present();
+    }
   }
 
 }
