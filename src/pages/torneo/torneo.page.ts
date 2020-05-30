2a6f265676ede8457b690321f0aa3fa7bbab3cd3
Javier Sintes <javier@secret-source.eu>
2020-05-30 21:09:06 +0100

    Add notifications number in tab

--- src/pages/torneo/torneo.page.ts
+++ src/pages/torneo/torneo.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NotificacionService} from '../../services/notificacion/notificacion.service';
+import {ActivatedRoute} from '@angular/router';
+import {UserService} from '../../services/user/user.service';
 
 @Component({
   selector: 'app-torneoo',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./torneo.page.scss'],
 })
 export class TorneoPage implements OnInit {
+  public notificacionesLength: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private userService: UserService,
+              private notificacionService: NotificacionService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const user = this.userService.getUser().getValue();
+      this.notificacionService.getNotificaciones(user, params.id).subscribe(notificaciones => {
+        this.notificacionesLength = notificaciones.length;
+      });
+    });
   }
-
 }
