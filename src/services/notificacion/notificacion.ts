61f2220d39db8dd0349171ed747510922c806cd2
Javier Sintes <javier@secret-source.eu>
2020-05-30 21:08:12 +0100

    Add notificacion interface

---
+++ src/services/notificacion/notificacion.ts
@@ -0,0 +1,10 @@
+export interface Notificacion {
+  idnotificacion: string;
+  idTorneo: string;
+  idpartido: string;
+  titulo: string;
+  descripcion: string;
+  fechahora: string;
+  estado: string;
+  timestamp: string;
+}
