ea3ae91626d92d539dee798e9a1af5a10bc1cb9a
Javier Sintes <javier@secret-source.eu>
2020-06-21 22:18:35 +0100

    Add restablecer-password and cambiar-password forms validation

--- src/app/pages/log-in/restablecer-password/restablecer-password.component.ts
+++ src/app/pages/log-in/restablecer-password/restablecer-password.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../../services/user/user.service';
-import {ModalController} from '@ionic/angular';
+import {AlertController, ModalController} from '@ionic/angular';
+import {ErrorService} from '../../../services/alert/error.service';
 
 @Component({
   selector: 'app-olvidar-password',
@@ -13,19 +14,45 @@ export class RestablecerPasswordComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
               private modalController: ModalController,
+              private alertController: AlertController,
+              private errorService: ErrorService,
               private userService: UserService) { }
 
   ngOnInit() {
     this.forgottenPasswordForm = this.formBuilder.group({
-      email: ['', Validators.required]
+      email: ['', [Validators.required, Validators.email]],
     });
   }
   async dismissModal() {
     await this.modalController.dismiss();
   }
+  get FormControl() {
+    return this.forgottenPasswordForm.controls;
+  }
   onSubmit(form) {
+    this.forgottenPasswordForm.markAllAsTouched();
     if (this.forgottenPasswordForm.status === 'VALID') {
-      this.userService.recoverPassword(form.email).subscribe(data => console.log(data));
+      this.userService.recoverPassword(form.email).subscribe(async () => {
+          const alert = await this.alertController.create({
+            header: 'Email enviado',
+            message: `Se ha enviado un correo a la dirección ${form.email}
+            con las instrucciones para restablecer la contraseña`,
+            buttons: [
+              {
+                text: 'OK',
+                role: 'cancel',
+                handler: async () => {
+                  await this.dismissModal();
+                }
+              }
+            ]
+          });
+          await alert.present();
+        },
+        async error => {
+          const alert = await this.errorService.createErrorAlert(error);
+          await alert.present();
+        });
     }
   }
 
