925405165e2bb256a5e4d7cd184a3a4051eca4f8
Javier Sintes <javier@secret-source.eu>
2020-06-24 22:25:11 +0100

    Add dismiss on navigation in select-categoria component

--- src/app/pages/torneo/stats/select-categoria/select-categoria.component.ts
+++ src/app/pages/torneo/stats/select-categoria/select-categoria.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {TorneoService} from '../../../../services/torneo/torneo.service';
 import {Categoria} from '../../../../services/torneo/categoria';
 import {PopoverController} from '@ionic/angular';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-select-categoria',
@@ -13,12 +14,17 @@ export class SelectCategoriaComponent implements OnInit {
   public categorias: Categoria[];
 
   constructor(private torneoService: TorneoService,
-              private popoverController: PopoverController) { }
+              private popoverController: PopoverController,
+              private router: Router) { }
   ngOnInit() {
     this.torneoService.getCategorias(this.idTorneo).subscribe(categorias => this.categorias = categorias);
   }
   async dismissPopover() {
     await this.popoverController.dismiss();
+  }
+  async navigateToCategoria(id: string) {
+    await this.dismissPopover();
+    await this.router.navigate([`torneo/${id}`]);
   }
 
 }
