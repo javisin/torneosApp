cbc682ef018fed13faf7cfa857c50a8692a8c47b
Javier Sintes <javier@secret-source.eu>
2020-06-25 20:46:38 +0100

    Add expired token message

--- src/app/services/error/error.service.ts
+++ src/app/services/error/error.service.ts
@@ -14,6 +14,9 @@ export class ErrorService {
               private storage: Storage,
               private router: Router) { }
   async createErrorAlert(error: string, status: number): Promise<HTMLIonAlertElement> {
+    if (status === 401) {
+      error = 'La sesión ha caducado, serás redirigido a la página de inicio de sesión.';
+    }
     return await this.alertController.create({
       header: 'Error',
       message: error,
